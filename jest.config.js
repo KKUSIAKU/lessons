e0605e5fb71fe170bda2b9d746941ae2a80c1c4c
kkusiaku <kusiaku.k@gmail.com>
2019-02-24 15:02:15 +0100

    start setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// check validConfig.js in npm 
+// source https://jestjs.io/docs/en/configuration
+
+module.exports = {
+  "moduleNameMapper": {
+    "^.*\\.scss$": "<rootDir>/jest.style.js",// replace any scss file with fake jest.style.js 
+  }
+};
---
+++ packages/input/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Input from './index';
+
+
+describe(' Input element test ', function(){
+  it(' Contain ', function(){
+    expect(false).toBe(true)
+  })
+})
